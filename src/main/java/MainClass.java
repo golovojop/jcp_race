df8fedd9f02ea21ed8f8fd34f061075b93bfe8a5
yagray <yagray@mail.ru>
2019-02-14 19:21:56 +0300

    Запускается

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,21 @@
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        CarOld[] cars = new CarOld[CARS_COUNT];
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new CarOld(race, 20 + (int) (Math.random() * 10));
+        }
+
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
