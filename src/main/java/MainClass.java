af6ab4cb0ce40e53f870b97b04eb17fa0159cd8b
yagray <yagray@mail.ru>
2019-02-15 09:31:54 +0300

    Добавил комменты

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,21 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainClass {
     public static final int CARS_COUNT = 4;
 
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
 
+        // Подготовить трассу
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
-        Car[] cars = new Car[CARS_COUNT];
 
-        for (int i = 0; i < cars.length; i++) {
-            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        // Дать старт
+        List<Thread> li = new ArrayList<>();
+        for(int i = 0; i < CARS_COUNT; i++) {
+            Thread t = new Thread(new Car(race, 20 + (int) (Math.random() * 10)));
+            li.add(t);
+            t.start();
         }
 
-        for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
-        }
-
+        // Ждем завершения
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        for(Thread t : li) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {e.printStackTrace();}
+        }
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
     }
 }
